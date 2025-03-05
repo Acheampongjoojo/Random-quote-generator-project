3d26a6ded9c0f58cc99ea6de3d7b05223dea4e34
Acheampongjoojo <acheampongjoojo29@gmail.com>
2025-03-05 15:18:11 -0800

    Update script.js

--- random quote generator/script.js
+++ random quote generator/script.js
@@ -1,8 +1,8 @@
 //Use an array to hold the value of the quotes
 const arrayOfQuotes = [
-    {'author': 'Theodore Rosevelt', 
-     'quote': 'Believe you can and you are halfway there',
-     'year': '1900'
+    {'author': 'Nelson Mandela', 
+     'quote': 'It always seems impossible until it is done',
+     'year': '1994'
 
     },
     {'author': 'Wayne Gretzky', 
@@ -21,9 +21,9 @@ const arrayOfQuotes = [
      'quote': 'Your time is limited, so do not waste it living someone else’s life.',
      'year': '2005'
     },
-    {'author': 'Hellen Keller', 
-     'quote': 'Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.',
-     'year': '1920'
+    {'author': 'Walt Disney', 
+     'quote': 'All our dreams can come true if we have the the courage to pursue them',
+     'year': '1957'
     },
     {'author': 'Eleanor Roosevelt', 
      'quote': 'The future belongs to those who believe in the beauty of their dreams.',
@@ -33,9 +33,9 @@ const arrayOfQuotes = [
      'quote': 'You don’t have to be great to start, but you have to start to be great.',
      'year': '1975'
     },
-    {'author': 'Nelson Mandela', 
-     'quote': 'It always seems impossible until it is done',
-     'year': '2001'
+    {'author': 'Maya Angelou', 
+     'quote': 'You will face many defeats in life but never let yourself be defeated',
+     'year': '1995'
     },
 ];
 
